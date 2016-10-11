c259948442c235146cca3f98141ae39e567e9a98
martyn pratt <martyn.pratt@kaplan.com>
2016-10-11 09:48:20 +0000

    corected port

--- app/app.py
+++ app/app.py
@@ -98,4 +98,4 @@ def create_application():
 # Start development web server
 application = create_application()
 if __name__=='__main__':
-    application.run(host='0.0.0.0', port=5000, debug=True)
+    application.run(host='0.0.0.0', port=8000, debug=True)
